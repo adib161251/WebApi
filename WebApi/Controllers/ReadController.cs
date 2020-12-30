6806183925e78f3930afe342a3e160ac01a42b42
Syed Adib Arman <adib.arman125@gmail.com>
2020-12-31 00:05:52 +0600

    uploading web api

---
+++ WebApi/Controllers/ReadController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class ReadController : Controller
+    {
+        // GET: Read
+        public ActionResult Read()
+        {
+            List<UserModel> users = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:49418/api/");
+
+                //geting data 
+                var responseTask = client.GetAsync("User/Getdata");
+                // responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<List<UserModel>>();
+                    readTask.Wait();
+
+                    users = readTask.Result;
+                }
+                else
+                {
+                    users = null;
+                }
+            }
+            return View("Read", users);
+        }
+    }
+}
---
+++ WebApi/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+
+        public UserModel()
+        {
+            this.Id = 0;
+            this.UserName = "Nothing";
+            this.Password = "Nothing";
+            this.Address = "Nothing";
+        }
+    }
+}
