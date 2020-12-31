78e7aeabe47295841d9e7acfa8002bb5ba436ad9
Syed Adib Arman <adib.arman125@gmail.com>
2020-12-31 10:21:18 +0600

    project from office

---
+++ WebApi/Controllers/ReadController.cs
@@ -0,0 +1,175 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class ReadController : Controller
+    {
+        // GET: Read
+        public ActionResult Read()
+        {
+            List<UserModel> users = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:49418/api/");
+
+                //geting data 
+                var responseTask = client.GetAsync("User/Getdata");
+                // responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<List<UserModel>>();
+                    readTask.Wait();
+
+                    users = readTask.Result;
+                }
+                else
+                {
+                    users = null;
+                }
+            }
+            return View("Read", users);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null) return RedirectToRoute("/Read/Read");
+            List<UserModel> user = null;
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:49418/api/");
+                var responseTask = client.GetAsync("User/Getdata/" + id);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if(result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<List<UserModel>>();
+                    readTask.Wait();
+
+                    user = readTask.Result;
+                }
+                else
+                {
+                    return Content("Error While getting data"+ result.StatusCode);
+                }
+
+            }
+            return View("Details",user);
+        }
+
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public ActionResult ProcessCreate(UserModel user)
+        {
+            List<UserModel> users = new List<UserModel>();
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:49418/api/");
+                
+                //this serializes model data into json formet
+                if(user.Id <= 0)
+                {
+                    var postTalk = client.PostAsJsonAsync<UserModel>("User/Postdata", user);
+                    postTalk.Wait();
+
+                    var result = postTalk.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        //Here Json data gets deserialize into List<model> data
+                        var readTask = result.Content.ReadAsAsync<List<UserModel>>();
+                        readTask.Wait();
+
+                        users = readTask.Result;
+                    }
+                    else
+                    {
+                        return Content("Problem while posting" + result.StatusCode);
+                    }
+                }
+
+                else
+                {
+                    var putTalk = client.PutAsJsonAsync<UserModel>("User/Updatedata", user);
+                    putTalk.Wait();
+
+                    var result = putTalk.Result;
+                    if(result.IsSuccessStatusCode)
+                    {
+                        var updateData = result.Content.ReadAsAsync<List<UserModel>>();
+                        updateData.Wait();
+
+                        users = updateData.Result;
+                    }
+                    else
+                    {
+                        return Content("Problem While Updating" + result.StatusCode);
+                    }
+                }
+               
+            }
+            return View("Read",users);
+        }
+
+
+        public ActionResult Update(int id)
+        {
+            List<UserModel> user = new List<UserModel>();
+            UserModel data = new UserModel();
+            //JObject json = new JObject();
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:49418/api/");
+                var responseTask = client.GetAsync("User/Getdata/" + id);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if(result.IsSuccessStatusCode)
+                {
+                    var readdata =result.Content.ReadAsAsync<List<UserModel>>();
+                    readdata.Wait();
+
+                    user = readdata.Result;
+                    foreach (UserModel i in user)
+                    {
+                        data.Id = i.Id;
+                        data.UserName = i.UserName;
+                        data.Password = i.Password;
+                        data.Address = i.Address;
+                    }
+                }
+                else
+                {
+                    return Content("Error with Uri" + result.StatusCode);
+                }
+            }
+                return View("Create",data);
+        }
+
+        public ActionResult Multi()
+        {
+            return View("Multi");
+        }
+
+        public ActionResult backup()
+        {
+            return View("backup");
+        }
+
+
+    }
+}
---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class UserController : ApiController
+    {
+        List<UserModel> users = new List<UserModel>()
+        {
+            new UserModel(){Id=1,UserName="Arman",Password="123",Address="dhaka"},
+            new UserModel(){Id=2,UserName="Arman1",Password="1231",Address="dhaka1"},
+            new UserModel(){Id=3,UserName="Arman2",Password="1232",Address="dhaka2"},
+            new UserModel(){Id=4,UserName="Arman3",Password="1233",Address="dhaka3"},
+            new UserModel(){Id=5,UserName="Arman4",Password="1234",Address="dhaka4"},
+            new UserModel(){Id=6,UserName="Arman5",Password="1235",Address="dhaka5"},
+        };
+
+        //get:/api/User/GetData
+        [Route("api/User/Getdata")]
+        [HttpGet]
+        public IHttpActionResult Getdata()
+        {
+            return Ok(users);
+        }
+        [Route("api/User/Getdata/{id}")]
+        [HttpGet]
+        public IHttpActionResult Getdata(int id)
+        {
+            //LINQ Query
+            var result = from data in users
+                         where data.Id == id
+                         select data;
+            //LINQ Method Syntax
+            var result1 = users.Where(x => x.Id == id).ToList();
+            if(result.ToList().Count != 0 )
+            {
+                return Ok(result);
+            }
+            return NotFound();
+
+        }
+
+        [Route("api/User/Postdata")]
+        [HttpPost]
+        public IHttpActionResult Postdata ([FromBody]UserModel user)
+        {
+            if (!ModelState.IsValid) return BadRequest("Data is invalid");
+
+
+            users.Add(user);
+            return Ok(users);
+        }
+
+        [Route("api/User/PostMultidata")]
+        [HttpPost]
+        public IHttpActionResult PostMultidata ([FromBody]List<UserModel> user)
+        {
+            if (!ModelState.IsValid) return BadRequest("Data is invalid");
+
+            users.AddRange(user);
+            return Ok(users);
+        }
+
+        [Route("api/User/Updatedata")]
+        [HttpPut]
+        public IHttpActionResult Updatedata([FromBody] UserModel user)
+        {
+            if (!ModelState.IsValid) return BadRequest("Data is Invalid");
+
+            var check = users.SingleOrDefault(x => x.Id == user.Id);
+            
+            if(check == null)
+            {
+                return NotFound();
+            }
+
+            else
+            {
+                users.Where(x => x.Id == user.Id).ToList().ForEach((x) => {
+                    x.Password = user.Password;
+                    x.UserName = user.UserName;
+                    x.Address = user.Address;
+                });
+
+                return Ok(users);
+            }
+        }
+        
+
+        [Route("api/User/Remove/{id}")]
+        [HttpDelete]
+        public IHttpActionResult Remove(int id)
+        {
+            //Here Single and First will through exception if there is no matching
+            var removeData = users.SingleOrDefault(x => x.Id == id); 
+            if(removeData == null)
+            {
+                return BadRequest("This Id doesn't Exists");
+            }
+            users.Remove(removeData);
+            return Ok(users);
+        }
+
+    }
+}
